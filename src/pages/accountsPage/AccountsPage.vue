239cd7357f35e0cb69523d5e38d90f59cc1d30ec
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2025-07-02 16:00:28 +0200

    feat: add AccountForm component to AccountsPage for account management

--- src/pages/accountsPage/AccountsPage.vue
+++ src/pages/accountsPage/AccountsPage.vue
@@ -1,7 +1,11 @@
 <template>
   <Button type="button" severity="contrast" icon="pi pi-plus-circle" />
+  <div>
+    <AccountForm />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Button } from 'primevue'
+import AccountForm from './components/AccountForm.vue'
 </script>
---
+++ src/pages/accountsPage/components/AccountForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="account-form">
+    <div class="field">
+      <label>Метка</label>
+      <InputText :maxlength="50" placeholder="Введите метки через ;" />
+    </div>
+    <div class="field">
+      <label>Тип записи</label>
+      <Select placeholder="Выберите тип" />
+    </div>
+    <div class="field">
+      <label>Логин *</label>
+      <InputText :maxlength="100" required placeholder="Логин" />
+    </div>
+    <div class="field">
+      <label>Пароль *</label>
+      <Password :maxlength="100" required toggleMask placeholder="Пароль" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Select, InputText, Password } from 'primevue'
+</script>
+<style scoped>
+.account-form {
+  display: flex;
+  gap: 10px;
+}
+.field {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  margin-bottom: 1rem;
+}
+</style>
