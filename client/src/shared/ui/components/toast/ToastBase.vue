bb0b77719294028424f492efffe5a36a80dd1d94
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2025-07-04 08:24:42 +0200

    feat: add ToastBase component and override styles for improved toast notifications

---
+++ client/src/shared/ui/components/toast/ToastBase.vue
@@ -0,0 +1,35 @@
+<template>
+  <Toast
+    position="top-center"
+    :pt="{
+      message: () => ({
+        style: {
+          display: 'flex',
+          padding: '10px',
+          width: 'max-content',
+          margin: '6px auto'
+        }
+      }),
+      messageText: () => ({
+        style: {
+          display: 'block',
+          color: 'var(--p-text-primary) !important'
+        }
+      }),
+      messageIcon: () => ({
+        style: {
+          display: 'none'
+        }
+      }),
+      detail: () => ({
+        style: {
+          'font-size': '16px'
+        }
+      })
+    }"
+  />
+</template>
+
+<script setup lang="ts">
+import { Toast } from 'primevue'
+</script>
---
+++ client/src/shared/ui/components/toast/override.ts
@@ -0,0 +1,36 @@
+export const toast = {
+  'border-radiis': 8,
+  'close-button-display': 'none',
+
+  colorScheme: {
+    light: {
+      root: {
+        error: {
+          background: '#D95575',
+          'detail-color': '#FFFFFF',
+          color: '#FFFFFF',
+          'border-color': 'transparent'
+        },
+        contrast: {
+          background: '#575A5B',
+          color: '#fff'
+        },
+        content: {
+          margin: 0,
+          padding: 10,
+          gap: 0
+        }
+      }
+    },
+    dark: {
+      root: {
+        error: {
+          background: '#D95575',
+          'detail-color': '#F0F0F0',
+          color: '#F0F0F0',
+          'border-color': 'transparent'
+        }
+      }
+    }
+  }
+}
