558308111f1906063c115cad9bbc53f3e77498e1
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2025-07-04 08:22:58 +0200

    feat: refactor AccountForm to use v-model for form fields and integrate validation

--- client/src/pages/accountsPage/components/AccountForm.vue
+++ client/src/pages/accountsPage/components/AccountForm.vue
@@ -2,7 +2,13 @@
   <div class="account-form">
     <div class="field">
       <label>Метка</label>
-      <InputText :maxlength="50" placeholder="Введите метки через ;" v-model="model!.label" />
+      <InputText
+        :maxlength="50"
+        placeholder="Введите метки через ;"
+        v-model="label"
+        @blur="onFieldBlur"
+      />
+      <div v-if="errors.login">{{ errors.label }}</div>
     </div>
     <div class="field">
       <label>Тип записи</label>
@@ -11,37 +17,72 @@
         :options="typeOptions"
         optionLabel="label"
         optionValue="value"
-        v-model="model!.type"
+        v-model="type"
+        @change="() => onFieldBlur()"
       />
+      <div v-if="errors.login">{{ errors.type }}</div>
     </div>
     <div class="field">
       <label>Логин *</label>
-      <InputText :maxlength="100" required placeholder="Логин" v-model="model!.login" />
+      <InputText
+        :maxlength="100"
+        required
+        placeholder="Логин"
+        v-model="login"
+        @blur="onFieldBlur"
+      />
+      <div v-if="errors.login">{{ errors.login }}</div>
     </div>
-    <div class="field">
+    <div v-if="type === 'local'" class="field">
       <label>Пароль *</label>
       <Password
         :maxlength="100"
         required
         toggleMask
         placeholder="Пароль"
-        v-model="model!.password"
+        v-model="password"
+        @blur="onFieldBlur"
       />
+      <div v-if="errors.login">{{ errors.password }}</div>
     </div>
-    <Button type="button" severity="contrast" icon="pi pi-trash" />
+    <Button type="button" severity="contrast" icon="pi pi-trash" @click="emit('remove')" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { Select, InputText, Password, Button } from 'primevue'
+import { useForm } from 'vee-validate'
+import { accountSchema } from '../utils/validations'
 import { typeOptions } from '../utils'
-
-const model = defineModel<{
-  label: string
-  type: string
-  login: string
-  password: string | null
+import type { IAccountForm } from '../types'
+import { watch } from 'vue'
+const props = defineProps<{
+  initialValues: IAccountForm
 }>()
+const emit = defineEmits<{
+  (e: 'remove'): void
+  (e: 'valid', payload: IAccountForm): void
+}>()
+
+const { handleSubmit, defineField, errors, resetForm } = useForm({
+  validationSchema: accountSchema,
+  initialValues: props.initialValues,
+})
+const [login] = defineField('login')
+const [password] = defineField('password')
+const [type] = defineField('type')
+const [label] = defineField('label')
+
+watch(
+  () => props.initialValues,
+  (val) => {
+    resetForm({ values: val })
+  },
+  { immediate: true, deep: true },
+)
+const onFieldBlur = handleSubmit((values) => {
+  emit('valid', values)
+})
 </script>
 <style scoped>
 .account-form {
