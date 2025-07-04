a7948da96d577de598faa260333924b072028671
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2025-07-04 08:24:04 +0200

    feat: refactor AccountsPage to improve account rendering and validation handling

--- client/src/pages/accountsPage/AccountsPage.vue
+++ client/src/pages/accountsPage/AccountsPage.vue
@@ -1,29 +1,57 @@
 <template>
-  <Button type="button" severity="contrast" icon="pi pi-plus-circle" />
+  <Button
+    type="button"
+    severity="contrast"
+    icon="pi pi-plus-circle"
+    @click="store.addEmtyAccount"
+  />
   <div>
-   <div v-for="(account, idx) in store.accounts" :key="account.login || idx">
-    <AccountForm v-model="store.accounts[idx]" />
-  </div>
+    <div v-for="(account, idx) in accountsForForm" :key="account.id">
+      <AccountForm
+        :initial-values="account"
+        @remove="store.removeAccount(account.id)"
+        @valid="onValid(account.id, $event)"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed, onMounted } from 'vue'
 import { Button } from 'primevue'
 import AccountForm from './components/AccountForm.vue'
 import { useAccountsStore } from '../store/AccountsStore'
+import type { IAccount, IAccountForm } from './types'
 const store = useAccountsStore()
 
+const accountsForForm = computed(() =>
+  store.accounts.map((acc) => ({
+    ...acc,
+    label: acc.label.map((l) => l.text).join('; '),
+  })),
+)
 onMounted(() => {
   store.fetchAccounts()
 })
 
-console.log(store.accounts);
+const parseLabel = (raw: string) => {
+  return raw
+    .split(';')
+    .map((s) => s.trim())
+    .filter(Boolean)
+    .map((text) => ({ text }))
+}
+const onValid = async (id: string, values: IAccountForm) => {
+  const password = values.type === 'ldap' ? null : values.password
+  const label = parseLabel(values.label)
+  const accountToSave: IAccount = {
+    ...values,
+    id,
+    password,
+    label,
+  }
 
-const newAccount = ref({
-  label: '',
-  type: '',
-  login: '',
-  password: '',
-})
+  store.updateAccount(accountToSave)
+  await store.saveAccountToServer(accountToSave)
+}
 </script>
