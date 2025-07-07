c617a75eb04c6c2cba25720a01cada8111550cc5
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2025-07-07 17:47:50 +0200

    feat: update accounts.json structure and change server port configuration

--- server/index.js
+++ server/index.js
@@ -51,4 +51,6 @@ app.delete("/accounts/:id", (req, res) => {
   res.status(204).end();
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
